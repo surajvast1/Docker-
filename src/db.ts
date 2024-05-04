9d9a25ed3ed8a710bd83d4d2168b825d531e0b49
Shubham Shrivastava <shrivastava.is21@bmsce.ac.in>
2024-05-04 22:31:03 +0530

    Docker learn
    Signed-off-by: Shubham Shrivastava <shrivastava.is21@bmsce.ac.in>

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://mongo:27017/myDatabase';
+const mongoUrl: string = 'mongodb://localhost:27017/myDatabase';                                      
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl)
